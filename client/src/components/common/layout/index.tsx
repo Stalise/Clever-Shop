04685e92b60ee2300e869a7cbf75fe58c1534308
Stas Liseenko <SLiseenko@alfabank.ru>
2023-04-04 18:32:51 +0300

    update typescript settings for client

---
+++ client/src/components/common/layout/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const Layout: FC<IProps> = ({ children }) => (
+  <>
+    <main>{children}</main>
+  </>
+);
