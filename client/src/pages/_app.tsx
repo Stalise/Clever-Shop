c826a04f8c83aee89cd3887a1b98e5c1c82e0f0c
ilonaK <ikosko@alfabank.ru>
2023-04-25 11:24:38 +0300

    feat(font): font optimization

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,27 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+import Head from 'next/head';
 
 import { Layout } from 'components/common/layout';
 
 import '../styles/globals.css';
 
+const spartanFont = localFont({
+    src: '../font/Spartan.ttf',
+});
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+    <React.Fragment>
+        <Head>
+            <title>Clever-Shop</title>
+        </Head>
+        <div className={spartanFont.className}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </div>
+    </React.Fragment>
 );
 
 export default App;
