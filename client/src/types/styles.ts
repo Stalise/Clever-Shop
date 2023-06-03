559d7e2fca3534324a9c39aad6fee729108df12f
Stas Liseenko <SLiseenko@alfabank.ru>
2023-06-03 16:37:39 +0300

    refactor(styles): update color variables names and add type for them

---
+++ client/src/types/styles.ts
@@ -0,0 +1,12 @@
+export type ColorsType =
+    | 'var(--dark)'
+    | 'var(--dark-grey)'
+    | 'var(--grey)'
+    | 'var(--light)'
+    | 'var(--light-grey)'
+    | 'var(--pink)'
+    | 'var(--yellow)'
+    | 'var(--red)'
+    | 'var(--green)'
+    | 'var(--dark-04)'
+    | 'var(--light-08)';
