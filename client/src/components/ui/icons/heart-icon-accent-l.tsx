0831178112501b1e075f60a4bd55da11cadc8186
Stas Liseenko <SLiseenko@alfabank.ru>
2023-06-15 02:07:42 +0300

    feat(icons): add svg icons

---
+++ client/src/components/ui/icons/heart-icon-accent-l.tsx
@@ -0,0 +1,18 @@
+// This is auto-generated file by scripts/convert-svg
+import React, { SVGProps } from 'react';
+
+const SvgHeartIconAccentL = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        role='img'
+        fill='currentColor'
+        focusable='false'
+        {...props}
+    >
+        <path d='M4.318 6.318a4.5 4.5 0 0 0 0 6.364L12 20.364l7.682-7.682a4.5 4.5 0 1 0-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 0 0-6.364 0Z' />
+    </svg>
+);
+export default SvgHeartIconAccentL;
