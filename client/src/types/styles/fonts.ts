978a62bb44ea797133072bd7f718dfc4cb83aa9c
Stalise <stas.liseenko@yahoo.com>
2023-08-18 01:51:43 +0300

    feat(fonts): update types for sizes

--- client/src/types/styles/fonts.ts
+++ client/src/types/styles/fonts.ts
@@ -1,2 +1,2 @@
-export type FontSizeSpartan = 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl';
+export type FontSizeSpartan = 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl';
 export type FontWeightSpartan = '100' | '200' | '300' | '400' | '500' | '600';
