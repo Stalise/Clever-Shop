269d75a5ba1aa8b4195e36c1da80d81cff42020e
Stalise <stas.liseenko@yahoo.com>
2023-08-28 17:26:04 +0300

    feat(styled-components): add props validation with @emotion/is-prop-valid package

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
 
+import isValid from '@emotion/is-prop-valid';
+import { StyleSheetManager } from 'styled-components';
+
 import GlobalStyles from 'assets/styles/global-styles';
 
 import { Layout } from 'components/common/layout';
 
+StyleSheetManager;
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
     <React.Fragment>
-        <GlobalStyles />
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <StyleSheetManager shouldForwardProp={isValid}>
+            <GlobalStyles />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </StyleSheetManager>
     </React.Fragment>
 );
 
