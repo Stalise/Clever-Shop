c288c55e3d43711b381e4978c44c74cd70ce535c
Stalise <stas.liseenko@yahoo.com>
2023-09-01 15:28:44 +0300

    feat(styles): remove @emotion/is-prop-valid and styleSheetManager

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,23 +1,16 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
 
-import isValid from '@emotion/is-prop-valid';
-import { StyleSheetManager } from 'styled-components';
-
 import GlobalStyles from 'assets/styles/global-styles';
 
 import { Layout } from 'components/common/layout';
 
-StyleSheetManager;
-
 const App: FC<AppProps> = ({ Component, pageProps }) => (
     <React.Fragment>
-        <StyleSheetManager shouldForwardProp={isValid}>
-            <GlobalStyles />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </StyleSheetManager>
+        <GlobalStyles />
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     </React.Fragment>
 );
 
