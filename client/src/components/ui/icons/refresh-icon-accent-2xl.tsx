aefa5e4476977083dc38d04ea9681a2c96b230c9
Stalise <stas.liseenko@yahoo.com>
2023-09-01 18:13:22 +0300

    feat(icons): update sizes

--- client/src/components/ui/icons/refresh-icon-accent-2xl.tsx
+++ client/src/components/ui/icons/refresh-icon-accent-2xl.tsx
@@ -1,7 +1,7 @@
 // This is auto-generated file by scripts/convert-svg
 import React, { SVGProps } from 'react';
 
-const SvgRefreshIconAccentXl = (props: SVGProps<SVGSVGElement>) => (
+const SvgRefreshIconAccent2Xl = (props: SVGProps<SVGSVGElement>) => (
     <svg
         xmlns='http://www.w3.org/2000/svg'
         width={40}
@@ -15,4 +15,4 @@ const SvgRefreshIconAccentXl = (props: SVGProps<SVGSVGElement>) => (
         <path d='M7.637 15h-.97V6.667L7.637 15Zm0 0a13.335 13.335 0 0 1 25.593 3.333M7.637 15H15m17.365 10h.968v8.333L32.365 25Zm0 0A13.338 13.338 0 0 1 6.77 21.667M32.365 25H25' />
     </svg>
 );
-export default SvgRefreshIconAccentXl;
+export default SvgRefreshIconAccent2Xl;
--- client/src/components/ui/icons/truck-icon-accent-2xl.tsx
+++ client/src/components/ui/icons/truck-icon-accent-2xl.tsx
@@ -1,7 +1,7 @@
 // This is auto-generated file by scripts/convert-svg
 import React, { SVGProps } from 'react';
 
-const SvgTruckIconAccentXl = (props: SVGProps<SVGSVGElement>) => (
+const SvgTruckIconAccent2Xl = (props: SVGProps<SVGSVGElement>) => (
     <svg
         xmlns='http://www.w3.org/2000/svg'
         width={40}
@@ -15,4 +15,4 @@ const SvgTruckIconAccentXl = (props: SVGProps<SVGSVGElement>) => (
         <path d='M21.667 26.667V10A1.667 1.667 0 0 0 20 8.333H6.667A1.667 1.667 0 0 0 5 10v16.667a1.667 1.667 0 0 0 1.667 1.666h1.666m13.334-1.666A1.666 1.666 0 0 1 20 28.333h-5m6.667-1.666V13.333a1.667 1.667 0 0 1 1.666-1.666h4.31c.442 0 .866.175 1.179.488l5.69 5.69c.312.313.488.736.488 1.178v7.644a1.666 1.666 0 0 1-1.667 1.666h-1.666m-10-1.666a1.667 1.667 0 0 0 1.666 1.666H25m-16.667 0a3.333 3.333 0 0 0 6.667 0m-6.667 0a3.333 3.333 0 1 1 6.667 0m16.667 0a3.333 3.333 0 0 1-6.667 0m6.667 0a3.333 3.333 0 1 0-6.667 0' />
     </svg>
 );
-export default SvgTruckIconAccentXl;
+export default SvgTruckIconAccent2Xl;
