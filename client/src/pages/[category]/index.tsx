525e8ff32585eed1437531ff3ff3f4c73f2b71e2
Stalise <stas.liseenko@yahoo.com>
2023-09-07 12:22:25 +0300

    refactor(structure): move CategoryPage component in screens folder

--- client/src/pages/[category]/index.tsx
+++ client/src/pages/[category]/index.tsx
@@ -1,28 +1,7 @@
 import { FC } from 'react';
-import { useRouter } from 'next/router';
 
-import { IBreadcrumbs } from 'types/breadcrumbs';
+import { Category } from 'screens/category';
 
-import { Intro } from 'components/common/intro';
+const CategoryPage: FC = () => <Category />;
 
-const Category: FC = () => {
-    const {
-        query: { category },
-    } = useRouter();
-
-    const breadcrumbs: IBreadcrumbs = [
-        { text: 'Home', path: '/' },
-        { text: category as string },
-    ];
-
-    return (
-        <div>
-            <Intro
-                breadcrumbs={breadcrumbs}
-                bottomContent={{ rating: 4, countReviews: 2 }}
-            />
-        </div>
-    );
-};
-
-export default Category;
+export default CategoryPage;
