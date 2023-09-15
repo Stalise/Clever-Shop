39be85f27b01b803ea6b6030ee512a024828ae97
Stalise <stas.liseenko@yahoo.com>
2023-09-15 17:40:23 +0300

    feat(product-page): add intro

--- client/src/screens/product/index.tsx
+++ client/src/screens/product/index.tsx
@@ -1,5 +1,37 @@
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
+import { NextRouter, withRouter } from 'next/router';
 
-export const Product: FC = () => {
-    return <div>Product</div>;
+import { productsMock } from 'data/products-mock';
+import type { IBreadcrumb } from 'types/breadcrumbs';
+
+import { Intro } from 'components/common/intro';
+
+interface IProps {
+    router: NextRouter;
+}
+
+const { rating, reviews } = productsMock.women[0];
+
+const Component: FC<IProps> = ({ router }) => {
+    const { category, id } = router.query;
+
+    const breadcrumbs: IBreadcrumb[] = [
+        { text: 'Home', path: '/' },
+        { text: category, path: `/${category}` },
+        { text: id },
+    ];
+
+    return (
+        <Fragment>
+            <Intro
+                breadcrumbs={breadcrumbs}
+                bottomContent={{
+                    rating,
+                    countReviews: reviews.length,
+                }}
+            />
+        </Fragment>
+    );
 };
+
+export const Product = withRouter(Component);
