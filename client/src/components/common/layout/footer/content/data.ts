4e7d35dbf00eb4f53d66374df6ec8c833f270aa9
Stalise <stas.liseenko@yahoo.com>
2023-09-17 01:46:34 +0300

    fix(links): change paths

--- client/src/components/common/layout/footer/content/data.ts
+++ client/src/components/common/layout/footer/content/data.ts
@@ -7,28 +7,28 @@ export const categories = {
     data: [
         { path: 'men', text: 'Men' },
         { path: 'women', text: 'Women' },
-        { path: '/', text: 'Accessories' },
-        { path: '/', text: 'Beauty' },
+        { path: '#!', text: 'Accessories' },
+        { path: '#!', text: 'Beauty' },
     ],
 };
 
 export const information = {
     title: 'Information',
     data: [
-        { path: '/', text: 'About Us' },
-        { path: '/', text: 'Contact Us' },
-        { path: '/', text: 'Blog' },
-        { path: '/', text: 'FAQs' },
+        { path: '#!', text: 'About Us' },
+        { path: '#!', text: 'Contact Us' },
+        { path: '#!', text: 'Blog' },
+        { path: '#!', text: 'FAQs' },
     ],
 };
 
 export const useful = {
     title: 'Useful links',
     data: [
-        { path: '/', text: 'Terms & Conditions' },
-        { path: '/', text: 'Returns & Exchanges' },
-        { path: '/', text: 'Shipping & Delivery' },
-        { path: '/', text: 'Privacy Policy' },
+        { path: '#!', text: 'Terms & Conditions' },
+        { path: '#!', text: 'Returns & Exchanges' },
+        { path: '#!', text: 'Shipping & Delivery' },
+        { path: '#!', text: 'Privacy Policy' },
     ],
 };
 
--- client/src/components/common/layout/header/bottom/navigation/index.tsx
+++ client/src/components/common/layout/header/bottom/navigation/index.tsx
@@ -4,13 +4,13 @@ import { useRouter } from 'next/router';
 import { StyledLink, Wrapper } from './styles';
 
 const data = [
-    { text: 'About Us', path: '/' },
+    { text: 'About Us', path: '#!' },
     { text: 'Women', path: 'women' },
     { text: 'Men', path: 'men' },
-    { text: 'Beauty', path: '/' },
-    { text: 'Accessories', path: '/' },
-    { text: 'Blog', path: '/' },
-    { text: 'Contact', path: '/' },
+    { text: 'Beauty', path: '#!' },
+    { text: 'Accessories', path: '#!' },
+    { text: 'Blog', path: '#!' },
+    { text: 'Contact', path: '#!' },
 ];
 
 export const Navigation: FC = () => {
