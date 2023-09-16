fadf372a0176fe613f18ff0e7d49af633ca09d07
Stalise <stas.liseenko@yahoo.com>
2023-09-16 18:53:33 +0300

    feat(info): create template

---
+++ client/src/components/product-page/content/info/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+import { Wrapper } from './styles';
+
+export const Info: FC = () => {
+
+    return (
+        <Wrapper>Info</Wrapper>
+    );
+};
---
+++ client/src/components/product-page/content/info/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 53%;
+    border: 1px solid red;
+`;
