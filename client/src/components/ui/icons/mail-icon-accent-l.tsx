fc58a5246d09ee2713079bfab1ffa1d6b4b9e17b
Stalise <stas.liseenko@yahoo.com>
2023-09-18 23:52:55 +0300

    feat(ui): add new icons

---
+++ client/src/components/ui/icons/mail-icon-accent-l.tsx
@@ -0,0 +1,18 @@
+// This is auto-generated file by scripts/convert-svg
+import React, { SVGProps } from 'react';
+
+const SvgMailIconAccentL = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        role='img'
+        fill='currentColor'
+        focusable='false'
+        {...props}
+    >
+        <path d='m3 8 7.89 5.26a2 2 0 0 0 2.22 0L21 8M5 19h14a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2Z' />
+    </svg>
+);
+export default SvgMailIconAccentL;
---
+++ client/src/components/ui/icons/refresh-icon-accent-l.tsx
@@ -0,0 +1,18 @@
+// This is auto-generated file by scripts/convert-svg
+import React, { SVGProps } from 'react';
+
+const SvgRefreshIconAccentL = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width={24}
+        height={24}
+        viewBox='0 0 24 24'
+        role='img'
+        fill='currentColor'
+        focusable='false'
+        {...props}
+    >
+        <path d='M4.582 9H4V4l.582 5Zm0 0a8.001 8.001 0 0 1 15.356 2M4.582 9H9m10.419 6H20v5l-.581-5Zm0 0a8.003 8.003 0 0 1-15.357-2m15.357 2H15' />
+    </svg>
+);
+export default SvgRefreshIconAccentL;
