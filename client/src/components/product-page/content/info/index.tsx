57d991411e049678b721ad2ee1ab8e6b3f48dd08
Stalise <stas.liseenko@yahoo.com>
2023-09-19 10:04:02 +0300

    feat(product-page): add description section

--- client/src/components/product-page/content/info/index.tsx
+++ client/src/components/product-page/content/info/index.tsx
@@ -4,6 +4,7 @@ import type { IProduct } from 'types/common';
 
 import { Actions } from './actions';
 import { Colors } from './colors';
+import { Description } from './description';
 import { Interaction } from './interaction';
 import { Payment } from './payment';
 import { Sizes } from './sizes';
@@ -32,5 +33,6 @@ export const Info: FC<IProps> = ({
         <Actions price={price} discount={discount} />
         <Interaction />
         <Payment />
+        <Description />
     </Wrapper>
 );
